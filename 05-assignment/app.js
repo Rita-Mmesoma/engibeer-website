a40874475026fbadc8c2dd711c96628e91d1dbf1
Rita-Mmesoma <ritammesoma357@gmail.com>
2025-08-07 08:52:11 +0100

    change the javascript function to classList

--- 05-assignment/app.js
+++ 05-assignment/app.js
@@ -1,13 +1,46 @@
-function showMenu(){
-    const menu = document.querySelector('.menu');
-    menu.style.display = 'flex';
-    const menuBar = document.querySelector('.menu-bar');
-    menuBar.style.display = 'none'
-}
+const windowWidth = window.innerWidth;
+const openMenu = document.querySelector('.btnOpen');
+const closeMenu = document.querySelector('.btnClose');
+const menu = document.querySelector('.menu');
 
-function hideMenu(){
-    const menu = document.querySelector('.menu');
-    menu.style.display = 'none';
-    const menuBar = document.querySelector('.menu-bar');
-    menuBar.style.display = 'block'
-}
+openMenu.addEventListener('click', () => {
+    menu.classList.toggle('open')
+})
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.remove('open')
+})
+
+
+
+
+// if(windowWidth > 700){
+//     menu.style.display = 'none';
+   
+// }
+
+// function showMenu(){
+   
+//     if(windowWidth < 600){
+//         const menu = document.querySelector('.menu');
+//         menu.style.display = 'flex';
+        
+//         const menuBar = document.querySelector('.menu-bar');
+//         menuBar.style.display = 'none'
+//     }
+//     /* const menuBar = document.querySelector('.menu-bar');
+//         menuBar.style.display = 'none' */
+// }
+// showMenu()
+
+// function hideMenu(){
+//     const menu = document.querySelector('.menu');
+//     menu.style.display = 'none';
+//     const menuBar = document.querySelector('.menu-bar');
+//     menuBar.style.display = 'block'
+// }
+
+
+
+
+
