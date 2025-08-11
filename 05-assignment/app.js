a5269a27f4f95bd87dcf04457def22edfbc043bc
Rita-Mmesoma <ritammesoma357@gmail.com>
2025-08-11 10:29:58 +0100

    used setInterval to remove the menu

--- 05-assignment/app.js
+++ 05-assignment/app.js
@@ -1,5 +1,54 @@
-const windowWidth = window.innerWidth;
-const openMenu = document.querySelector('.btnOpen');
+let windowWidth = window.innerWidth;
+const menu = document.querySelector('.menu');
+const menuBar = document.querySelector('.menu-bar');
+const main = document.querySelector('.main');
+
+
+function showMenu(){
+    menu.style.display = 'flex';
+    menuBar.style.display = 'none';
+}
+
+
+function hideMenu(){
+    menu.style.display = 'none';
+    menuBar.style.display = 'block'
+}
+
+setInterval(() => {
+    if(window.innerWidth > 900){
+        menu.style.display = 'none';
+        menuBar.style.display = 'none';
+    }else{
+        menuBar.style.display = 'block';
+    }
+}, 100)
+
+/* window.onresize = function(){
+    const width = window.innerWidth;
+    if(width > 900){
+        menu.style.display = 'none';
+        menuBar.style.display = 'none'
+    }
+} */
+
+
+
+/* function checkinMenu(){
+    if(window.innerWidth > 900){
+        menu.style.display = 'none';
+        console.log("Firing...: ", window.innerWidth)
+    }
+} */
+/* if(windowWidth < 500){
+    main.style.backgroundColor = 'red';
+}else{
+    main.style.backgroundColor = 'white';
+} */
+
+
+
+/* const openMenu = document.querySelector('.btnOpen');
 const closeMenu = document.querySelector('.btnClose');
 const menu = document.querySelector('.menu');
 
@@ -9,38 +58,4 @@ openMenu.addEventListener('click', () => {
 
 closeMenu.addEventListener('click', () => {
     menu.classList.remove('open')
-})
-
-
-
-
-// if(windowWidth > 700){
-//     menu.style.display = 'none';
-   
-// }
-
-// function showMenu(){
-   
-//     if(windowWidth < 600){
-//         const menu = document.querySelector('.menu');
-//         menu.style.display = 'flex';
-        
-//         const menuBar = document.querySelector('.menu-bar');
-//         menuBar.style.display = 'none'
-//     }
-//     /* const menuBar = document.querySelector('.menu-bar');
-//         menuBar.style.display = 'none' */
-// }
-// showMenu()
-
-// function hideMenu(){
-//     const menu = document.querySelector('.menu');
-//     menu.style.display = 'none';
-//     const menuBar = document.querySelector('.menu-bar');
-//     menuBar.style.display = 'block'
-// }
-
-
-
-
-
+}) */
